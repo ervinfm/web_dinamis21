9842d9bfe2300d234ba0e02af774fe063b435bdd
ervinfm <ervinfm14@gmail.com>
2021-10-25 17:51:31 +0700

    Database Insert

---
+++ pertemuan_3/akademik.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 25 Okt 2021 pada 12.50
+-- Versi server: 10.4.16-MariaDB
+-- Versi PHP: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(10) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jkel` varchar(1) NOT NULL,
+  `alamat` text NOT NULL,
+  `tgllhr` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `jkel`, `alamat`, `tgllhr`) VALUES
+('1700018127', 'Ervin Fikot M', 'L', 'Kabupaten Rejang Lebong, Bengkulu 39118', '1999-07-14'),
+('1700018129', 'Muhammad Nashir', 'L', 'Bantul', '1999-08-17'),
+('1700018130', 'Eef Mekeliano', 'L', 'Belitung', '1999-08-17');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
